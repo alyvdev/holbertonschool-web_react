540ba03486824657e0fd82dd274592d7f44fb005
alyvdev <alyvofficial@gmail.com>
2025-05-29 18:29:07 +0400

    Add Director interface and implement StudentClass with methods for displayName and workOnHomework

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,42 @@ interface Teacher {
   location: string;
   [propName: string]: any;
 }
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstName, lastName) {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+export interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  displayName(): string;
+  workOnHomework(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
