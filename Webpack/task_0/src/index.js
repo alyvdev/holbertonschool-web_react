3ef14d8fbe541640782b010111b67a5374a530a4
alyvdev <alyvofficial@gmail.com>
2025-06-05 23:08:53 +0400

    feat: initialize Webpack project with jQuery and add basic HTML content

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+const $ = require("jquery");
+
+// Add three paragraphs to the page body
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
