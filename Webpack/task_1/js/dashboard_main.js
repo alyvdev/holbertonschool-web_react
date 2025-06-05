cf484e8a95c7940d30341a8a23afb23f4f893d60
alyvdev <alyvofficial@gmail.com>
2025-06-05 23:59:01 +0400

    refactor: standardize quotation marks in dashboard_main.js for consistency

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -12,7 +12,7 @@ $(function () {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
+  $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
   let debouncedFunc = _.debounce(() => {
