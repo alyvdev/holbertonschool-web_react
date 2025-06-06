2662046202ada20568d3d04c8c3dd4430fb7f69d
alyvdev <alyvofficial@gmail.com>
2025-06-06 13:18:00 +0400

    refactor(dashboard): improve code structure and button click handling
    - Reorder imports for better organization
    - Fix indentation consistency throughout the file
    - Update button markup to include span element
    - Simplify debounce function by removing unnecessary delay parameter
    - Improve counter variable handling in click handler

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,24 +1,25 @@
+import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
-import '../css/main.css';
 
 let count = 0;
 
 function updateCounter() {
-  count += 1;
-  return count;
+	count += 1;
+	return count;
 }
 
-$(function () {
-  $('body').append('<div id="logo"></div>');
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+$(function() {
+	$('body').append("<div id='logo'></div>");
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<button><span>Click here to get started<span></button>');
+	$('body').append("<p id='count'></p>");
+	$('body').append('<p>Copyright - Holberton School</p>');
 
-  let debouncedFunc = _.debounce(() => {
-    $('#count').text(`${updateCounter()} clicks on the button`);
-  }, 500);
-  $('button').on('click', debouncedFunc);
+	let debouncedFunc = _.debounce(() => {
+		let count = updateCounter();
+		$('#count').text(`${count} clicks on the button`);
+	});
+	$('button').on('click', debouncedFunc);
 });
