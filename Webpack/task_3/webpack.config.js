74200116a8507364006045c733e5487f7b8f8e13
alyvdev <alyvofficial@gmail.com>
2025-06-16 12:16:16 +0400

    refactor(webpack): simplify entry configuration and update output path

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,53 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-	// entry: {
-	// 	// lodash: './node_modules/lodash',
-	// 	// jquery: './node_modules/jquery',
-	// 	header: {
-	// 		import: './modules/header/header.js',
-	// 	},
-	// 	body: {
-	// 		import: './modules/body/body.js',
-	// 		// dependOn: 'jquery',
-	// 	},
-	// 	footer: {
-	// 		import: './modules/footer/footer.js',
-	// 	},
-	// 	// shared: 'lodash',
-	// },
-	entry: {
-    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
+  mode: "development",
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
   },
-	output: {
-		path: path.resolve(__dirname, '/public'),
-		filename: '[name].bundle.js',
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-      title: 'Holberton Dashboard',
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"), // Corrected path
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"), // Updated from contentBase
+    port: 8564,
+    open: true,
+  },
+  devtool: "inline-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Holberton Dashboard", // Optional: you can customize title or provide a template
     }),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-				use: ['file-loader'],
-			},
-		],
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './public',
-		port: 8564,
-	},
-	mode: 'development',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          "file-loader", // For handling image files
+          {
+            loader: "image-webpack-loader", // For optimizing images
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false, // Disable optipng for faster builds if not needed
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
